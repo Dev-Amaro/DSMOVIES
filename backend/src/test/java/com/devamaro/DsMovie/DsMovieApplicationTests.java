cd5b48c6c4d340ab4c8588d4ac39e6a14cc69302
Dev-Amaro <joao.guilherme1827@gmail.com>
2022-01-11 11:01:35 -0300

    Project created

---
+++ backend/src/test/java/com/devamaro/DsMovie/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devamaro.DsMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
